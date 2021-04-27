3387c1e7fbfcd5ff892d6251efe6a647dded7dfe
ishuen <12228644+ishuen@users.noreply.github.com>
2021-04-27 22:10:30 +0800

    generate data row count per hour

--- main.py
+++ main.py
@@ -4,13 +4,42 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+import sys
+import pandas as pd
+from collections import defaultdict
 
+
+def main(argv):
+    chunk_size = 10 ** 3
+    dictionary = defaultdict(int)
+    for chunk in pd.read_csv(argv[0], chunksize=chunk_size, header=None):
+        last_key = process(chunk, argv[2], dictionary)
+        if len(dictionary) > 1:
+            temp_dict_val = dictionary.pop(last_key)
+            statistics = pd.DataFrame.from_dict(dictionary, orient='index', columns=['count'])
+            statistics.to_csv(argv[1], mode='a', header=False)
+            dictionary = defaultdict(int)
+            dictionary[last_key] = temp_dict_val
+    statistics = pd.DataFrame.from_dict(dictionary, orient='index', columns=['count'])
+    statistics.to_csv(argv[1], mode='a', header=False)
+
+
+def process(chunk, col, dictionary):
+    timestamp_col = chunk[int(col)].to_numpy()
+    key = get_key(timestamp_col[0])
+    if key == get_key(timestamp_col[-1]):
+        dictionary[key] += len(timestamp_col)
+    else:
+        for timestamp in timestamp_col:
+            key = get_key(timestamp)
+            dictionary[key] += 1
+    return key
+
+
+def get_key(timestamp):
+    dt_object = pd.to_datetime(timestamp, unit='ms').to_pydatetime()
+    return dt_object.strftime("%b-%d-%Y %H:00")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main(sys.argv[1:])
---
+++ readme.md
@@ -0,0 +1,10 @@
+readme
+====
+
+### Usage
+
+```
+main.py <intput_csv_filename> <output_csv_filename> <column_num_of_timestamp>
+```
+
+column\_num\_of\_timestamp: starts from 0
